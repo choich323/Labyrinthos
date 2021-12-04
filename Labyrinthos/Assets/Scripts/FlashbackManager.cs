8c8b063fb1b05f4da2efa6965f4c205a3cb3963b
Jong Hyun Choi <choich323@naver.com>
2021-12-05 00:22:11 +0900

    Scene merge
    flash back2 + Endscene

---
+++ Labyrinthos/Assets/Scripts/FlashbackManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlashbackManager : MonoBehaviour
+{
+    public GameObject flashbackManger;
+    public int flashbackNum;
+
+    void Update()
+    {
+        if (!flashbackManger.activeSelf && flashbackNum == 2)
+            SceneManager.LoadScene("EndScene");
+    }
+}
